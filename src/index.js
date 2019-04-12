cff1ace6f60736e6dffbae37ce750e10ee388403
ccjmne <ccjmne@gmail.com>
2019-04-12 15:33:45 +0200

    Refactor code, implement multi-page documents

--- src/index.js
+++ src/index.js
@@ -1,28 +1,57 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const booleanParser = require('express-query-boolean');
 const numberParser = require('express-query-int');
+
+const merge = require('pdf-merge');
+const tmp = require('tmp');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(bodyParser.text({ type: 'text/html' }));
 app.use(booleanParser());
 app.use(numberParser());
 
+async function print({ browser, htmlContents, options }) {
+  const page = await browser.newPage();
+  await page.setContent(htmlContents, { waitUntil: 'networkidle0' });
+  return page.pdf(options);
+}
+
+function parseRequest(request) {
+  const { groups: { filename } } = (request.query.filename || 'document').match(/^(?<filename>.+?)(?:\.pdf)?$/);
+  return { filename, options: { format: 'a4', landscape: false, printBackground: true, ...request.query, path: null } }; // discard potential `path` parameter
+}
+
 export function use(puppeteer) {
-  app.post('/', async (request, response) => {
-    const browser = await puppeteer.launch({
+  function launchBrowser() {
+    return puppeteer.launch({
       headless: true,
       args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
+  }
 
-    const page = await browser.newPage();
-    await page.setContent(request.body.page, { waitUntil: 'networkidle0' });
-
-    const res = await page.pdf({ format: 'a4', landscape: false, printBackground: true, ...request.query });
+  app.post('/', async (request, response) => {
+    const browser = await launchBrowser();
+    const { filename, options } = parseRequest(request);
+    const res = await print({ htmlContents: request.body, browser, options });
     await browser.close();
+    response.attachment(`${filename}.pdf`).send(res);
+  });
 
-    const { groups: { filename } } = (request.query.filename || 'document').match(/^(?<filename>.+?)(?:\.pdf)?$/);
+  app.post('/multiple', async (request, response) => {
+    const browser = await launchBrowser();
+    const { filename, options } = parseRequest(request);
+    const files = await Promise.all(request.body.pages.map(htmlContents => {
+      const { name: path, removeCallback: rm } = tmp.fileSync();
+      return print({ htmlContents, browser, options: { ...options, path: path } }).then(() => ({ path, rm }));
+    }));
+
+    const res = await merge(files.map(({ path }) => path), { oupput: 'Stream' });
+    files.forEach(({ rm }) => rm());
+    await browser.close();
     response.attachment(`${filename}.pdf`).send(res);
   });
 
