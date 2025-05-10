2960c2d26b373f9ae57cd64133397e11c1dd94b5
Éric NICOLAS <ccjmne@gmail.com>
2025-05-10 02:28:24 +0200

    Reinstate compose.sh
    - Use docker/buildx to build images for both amd and arm
    - Update target registry to ghcr.io
    - Update pre-build procedure (to none!)
    - Update shebang to use /usr/bin/env
    - Fix resetting of shell output colour

--- compose.sh
+++ compose.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /usr/bin/env bash
 set -e
 
 ok() { printf "   [\e[0;32m ok \e[0m] $1\n"; }
@@ -23,27 +23,20 @@ catch() {
 }
 trap 'catch ${LINENO}' ERR
 
-registry=424880512736.dkr.ecr.eu-west-1.amazonaws.com
-img=puppeteer-html2pdf
+registry=ghcr.io
+img=ccjmne/puppeteer-html2pdf
 version=$1
 
-nf "Build Express server..."
-npm run build
+nf "Build and push Docker image..."
+docker buildx build --push --platform arm64,amd64 \
+  --tag ${registry}/${img}:${version} \
+  --tag ${registry}/${img}:latest .
 ok "Build complete"
-
-nf "Build Docker image..."
-docker build -t ${img}:${version} -t ${registry}/${img}:${version} -t ${img}:latest -t ${registry}/${img}:latest .
-ok "Build complete"
-
-nf "Push to our private container registry..."
-docker push ${registry}/${img}:${version}
-docker push ${registry}/${img}:latest
-ok "Push complete"
 
 nf "\e[2mTesting:\e[0m"
-nf "   docker run -it --rm -p=\e[1;34m<port>\e[0m:3000 ${img}:\e[1;34m${version}"
+nf "   docker run -it --rm -p=\e[1;34m<port>\e[0m:3000 ${registry}/${img}:\e[1;34m${version}\e[0m"
 nf "\e[0m - \e[2mkill w/: \e[0mCtrl^C"
 nf "\e[2mProduction:\e[0m"
 nf "   docker run --detach --memory 1G --sysctl net.ipv6.conf.all.disable_ipv6=1 \\
-                        --name ${img} -p=\e[1;34m<port>\e[0m:3000 ${img}:\e[1;34m${version}"
-nf "\e[0m - \e[2mstop w/: \e[0mdocker stop ${img}"
+                        --name html2pdf -p=\e[1;34m<port>\e[0m:3000 ${registry}/${img}:\e[1;34m${version}\e[0m"
+nf "\e[0m - \e[2mstop w/: \e[0mdocker stop html2pdf"
